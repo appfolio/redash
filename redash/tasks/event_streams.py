f46897fdb410f789e41afefe8a1a9dded10bd2c6
Daniel Ray <daniel.ray@appfolio.com>
2016-09-21 15:05:01 -0700

    dr - remove logic for creating streams
    (cherry picked from commit b2988fce5fa52854c68049ad564888f98232c915)
    (cherry picked from commit 91ea75aca1ecff470f89c5f22b550eadb00c55d3)

--- redash/tasks/event_streams.py
+++ redash/tasks/event_streams.py
@@ -15,7 +15,6 @@ FIREHOSE_BATCH_SIZE_LIMIT = 500
 @celery.task(name="redash.tasks.write_to_firehose", base=BaseTask)
 def write_to_firehose(query_result_id, stream_name, query_name):
     query_result = models.QueryResult.get_by_id(query_result_id)
-    ensure_delivery_stream_created(stream_name)
     write_batch_data_to_s3(stream_name, query_result.data, query_name)
 
     logger.info("Results of query '{}' written to event stream '{}'".format(query_name, stream_name))
@@ -33,36 +32,9 @@ def event_stream_callback_for(query_id):
         return None
 
 
-def ensure_delivery_stream_created(delivery_stream_name):
-    if delivery_stream_name not in [name for name in firehose.list_delivery_streams()['DeliveryStreamNames']]:
-        logger.info('Creating Firehose delivery stream %s' % delivery_stream_name)
-        firehose.create_delivery_stream(
-            DeliveryStreamName=delivery_stream_name,
-            S3DestinationConfiguration={
-                'RoleARN':  'arn:aws:iam::088684165182:role/firehose_delivery_role',
-                'BucketARN': 'arn:aws:s3:::appfolio-kinesis-test',
-                'Prefix': delivery_stream_name + '/',
-                'BufferingHints': {
-                    'SizeInMBs': 100,
-                    'IntervalInSeconds': 60
-                }
-            }
-        )
-
-    while firehose.describe_delivery_stream(DeliveryStreamName=delivery_stream_name)['DeliveryStreamDescription']['DeliveryStreamStatus'] == 'CREATING':
-        time.sleep(2)
-
-
-def write_batch_data_to_s3(delivery_stream_name, data, event_stream):
-    if delivery_stream_name in firehose.list_delivery_streams()['DeliveryStreamNames']:
-        desc = firehose.describe_delivery_stream(DeliveryStreamName=delivery_stream_name)
-        if desc['DeliveryStreamDescription']['DeliveryStreamStatus'] == 'ACTIVE':
-            for record_batch in parse_data_to_batches(data, event_stream):
-                firehose.put_record_batch(DeliveryStreamName=delivery_stream_name, Records=record_batch)
-        else:
-            logger.info('Waiting for AWS to finish creating your delivery_stream')
-    else:
-        logger.error('The stream %s does not exist' % delivery_stream_name)
+def write_batch_data_to_s3(stream_name, data, event_name):
+    for record_batch in parse_data_to_batches(data, event_name):
+        firehose.put_record_batch(DeliveryStreamName=stream_name, Records=record_batch)
 
 
 def parse_data_to_batches(query_data, event_name):
